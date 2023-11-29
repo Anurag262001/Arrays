12b50357f688468e72b59308aba68f7907cddc6b
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2023-11-30 00:45:21 +0530

    Add files via upload

---
+++ SecondLargestElement.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class SecondLargestElement{
+    public static void main(String[] args) {
+    //by sorting
+    int arr1[] = {1,2,5,3,7,5,9,23};
+    int temp;
+    for(int i=0;i<arr1.length;i++){
+        for(int j=0;j<arr1.length-1;j++){
+            if(arr1[j]>arr1[j+1]){
+                temp = arr1[j+1];
+                arr1[j+1]=arr1[j];
+                arr1[j] = temp;
+            }
+        }
+    }
+    System.out.println(arr1[arr1.length-2]);
+     
+
+    //my min_value
+    int arr[] = {1,3,2,45,3,5,7,6,498,5};
+    int seclargest = 0;
+    int firstlargest = Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]>firstlargest){
+            seclargest=firstlargest;
+            firstlargest=arr[i];
+        }
+    }
+    System.out.println(seclargest);
+        }
+    }    
